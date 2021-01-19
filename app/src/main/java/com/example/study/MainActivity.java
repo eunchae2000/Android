b0b08671ec695d91cd647d8c7eb173412bdd3107
eunchae2000 <tldjs3651@naver.com>
2021-01-19 13:51:16 +0900

    <ListView>를 사용해서 목차들을 list 형식으로 만들고 목차의 이름을 바꾸고 저장하는 함수를 사용해서 저장까지 완료

--- app/src/main/java/com/example/study/MainActivity.java
+++ app/src/main/java/com/example/study/MainActivity.java
@@ -5,10 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
     public class MainActivity extends AppCompatActivity {
 
@@ -19,11 +25,22 @@ import android.widget.Toast;
     private Button btn_move;
     private EditText et_test;
     private String str;
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = (ListView)findViewById(R.id.list);
+        List <String> data = new ArrayList<>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        list.setAdapter(adapter);
+
+        data.add("coco");
+        data.add("popo");
+        data.add("nono");
+        adapter.notifyDataSetChanged();
 
         et_id = findViewById(R.id.et_id);
         btn_test = findViewById(R.id.btn_test);

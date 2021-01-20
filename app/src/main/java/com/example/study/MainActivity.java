2cf9e6f537820b66f7de8c9a9ea3243e746a8916
eunchae2000 <tldjs3651@naver.com>
2021-01-20 15:38:22 +0900

    Android Studio로 카메라를 실행하는 기능 촬영이란 버튼을 누르면 휴대폰에 있는 기본 어플 카메라가 실행되면서 이미지 뷰를 볼 수 있음

--- app/src/main/java/com/example/study/MainActivity.java
+++ app/src/main/java/com/example/study/MainActivity.java
@@ -1,12 +1,23 @@
     package com.example.study;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
@@ -22,68 +33,126 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.SimpleTimeZone;
 
     public class MainActivity extends AppCompatActivity {
 
-        private DrawerLayout drawerLayout;
-        private View drawerView;
+        private static final int REQUEST_IMAGE_CAPTURE = 672;
+        private String imageFilePath;
+        private Uri photoUri;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
-            drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
-            drawerView = (View)findViewById(R.id.drawer);
+            // 권한 체크
+            TedPermission.with(getApplicationContext())
+                    .setPermissionListener(permissionListener)
+                    .setRationaleMessage("카메라 권한이 필요합니다.")
+                    .setDeniedMessage("거부하셨습니다.")
+                    .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
+                    .check();
 
-            Button btn_open = (Button)findViewById(R.id.btn_open);
-            btn_open.setOnClickListener(new View.OnClickListener(){
-
+            // 촬영 버튼을 클릭하면 실행되는 action
+            findViewById(R.id.btn_capture).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    drawerLayout.openDrawer(drawerView);
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if(intent.resolveActivity(getPackageManager())!= null){
+                        File photoFile = null;
+                        try {
+                            photoFile = createImageFile();
+                        }catch (IOException e){
+
+                        }
+                        if(photoFile != null){
+                            photoUri = FileProvider.getUriForFile(getApplicationContext(), getPackageName(), photoFile);
+                            intent.putExtra(MediaStore. EXTRA_OUTPUT, photoUri);
+                            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+                        }
+                    }
                 }
             });
 
-            Button btn_close = (Button)findViewById(R.id.btn_close);
-            btn_close.setOnClickListener((new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    drawerLayout.closeDrawers();
-                }
-            }));
-
-            drawerLayout.setDrawerListener(listener);
-            drawerView.setOnTouchListener(new View.OnTouchListener() {
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    return true;
-                }
-            });
         }
 
-        DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
-            @Override
-            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+        // 이미지 파일에 (연, 월, 일, 시간, 분, 초) 단위로 파일을 생성해서 이름이 중복되지 않게 하는 것
+        private File createImageFile() throws IOException {
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String imageFileName = "TEST_"+ timeStamp + "_";
+            File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+            File image = File.createTempFile(
+                    imageFileName,
+                    ".jpg",
+                    storageDir
+            );
+            imageFilePath = image.getAbsolutePath();
+            return image;
+        }
 
+        @SuppressLint("MissingSuperCall")
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                Bitmap bitmap = BitmapFactory.decodeFile(imageFilePath);
+                ExifInterface exif = null;
+
+                try {
+                    exif = new ExifInterface(imageFilePath);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                int exifOrientaion;
+                int exifDegree;
+
+                if (exif != null) {
+                    exifOrientaion = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                    exifDegree = exifOrientaionTodegress(exifOrientaion);
+                }else{
+                    exifDegree = 0;
+                }
+                ((ImageView)findViewById(R.id.iv_result)).setImageBitmap(rotate(bitmap,exifDegree));
+            }
+        }
+
+        // 이미지 회전에 대한 구문
+        private int exifOrientaionTodegress(int exifOrientation){
+            if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90){
+                return 90;
+            }else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180){
+                return 180;
+            }else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270){
+                return 270;
+            }
+            return 0;
+        }
+
+        private Bitmap rotate(Bitmap bitmap, float degree){
+            Matrix matrix = new Matrix();
+            matrix.postRotate(degree);
+            return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+        }
+
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                Toast.makeText(getApplicationContext(), "권한이 허용됨", Toast.LENGTH_SHORT).show();
             }
 
             @Override
-            public void onDrawerOpened(@NonNull View drawerView) {
-
-            }
-
-            @Override
-            public void onDrawerClosed(@NonNull View drawerView) {
-
-            }
-
-            @Override
-            public void onDrawerStateChanged(int newState) {
-
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                Toast.makeText(getApplicationContext(), "권한이 거부됨", Toast.LENGTH_SHORT).show();
             }
         };
     }

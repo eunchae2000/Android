bc61a9b5347b48b95aaa6916dab4f3441f25a049
eunchae2000 <tldjs3651@naver.com>
2021-01-29 15:45:01 +0900

    Android Studio로 Log  출력과 주석다는 방법/ 디버그

--- app/src/main/java/com/example/study/MainActivity.java
+++ app/src/main/java/com/example/study/MainActivity.java
@@ -22,6 +22,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
@@ -47,60 +48,25 @@ import java.util.SimpleTimeZone;
 
     public class MainActivity extends AppCompatActivity {
 
-        Button btn1, btn2, btn3, btn4;
-
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
-            btn1 = (Button)findViewById(R.id.btn1);
-            btn2 = (Button)findViewById(R.id.btn2);
-            btn3 = (Button)findViewById(R.id.btn3);
-            btn4 = (Button)findViewById(R.id.btn4);
 
-            btn1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                    Fragment1 fragment1 = new Fragment1();
-                    transaction.replace(R.id.fram, fragment1);
-                    transaction.addToBackStack(null);
-                    transaction.commit();       // 새로고침과 같은 기능
-                }
-            });
+            String hong = "Android Studio";
 
-            btn2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                    Fragment2 fragment2 = new Fragment2();
-                    transaction.replace(R.id.fram, fragment2);
-                    transaction.addToBackStack(null);
-                    transaction.commit();       // 새로고침과 같은 기능
-                }
-            });
+            Log.e("MainActivity: ", hong);
 
-            btn3.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                    Fragment3 fragment3 = new Fragment3();
-                    transaction.replace(R.id.fram, fragment3);
-                    transaction.addToBackStack(null);
-                    transaction.commit();       // 새로고침과 같은 기능
-                }
-            });
+            // 한 줄 주석
 
-            btn4.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                    Fragment4 fragment4 = new Fragment4();
-                    transaction.replace(R.id.fram, fragment4);
-                    transaction.addToBackStack(null);
-                    transaction.commit();       // 새로고침과 같은 기능
-                }
-            });
+            /*
+            여러 줄 주석
+             */
 
+            int a = 10;
+
+
+            // 로그를 찍었다.
+            Log.e("MainActivity", String.valueOf(a));
         }
     }

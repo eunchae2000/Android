ee684b0d86a4fc3fe57fd5f165fc84e7081074fd
eunchae2000 <tldjs3651@naver.com>
2021-02-01 17:41:24 +0900

    Android Studio를 이용해서 계산기 만들기 - 1 <안드로이드 특강>

--- app/src/main/java/com/example/study/MainActivity.java
+++ app/src/main/java/com/example/study/MainActivity.java
@@ -3,8 +3,13 @@
     import android.os.Bundle;
     import android.os.Handler;
     import android.os.Message;
+    import android.view.MotionEvent;
     import android.view.View;
+    import android.view.inputmethod.InputMethod;
+    import android.view.inputmethod.InputMethodManager;
     import android.widget.Button;
+    import android.widget.EditText;
+    import android.widget.TextView;
     import android.widget.Toast;
 
     import androidx.annotation.NonNull;
@@ -12,54 +17,112 @@
 
     public class MainActivity extends AppCompatActivity {
 
-        Button btn_start, btn_stop;
-        Thread thread;
-        boolean isThread = false;
+        EditText edit1, edit2;
+        Button btnAdd, btnSub, btnMul, btnDiv;
+        TextView textResult;
+        String num1;
+        String num2;
+        double result;
+
+        Button[] numButtons = new Button[10];
+        int[] numBtnIDs = { R.id.button0, R.id.button1, R.id.button2,
+                R.id.button3, R.id.button4, R.id.button5, R.id.button6,
+                R.id.button7, R.id.button8, R.id.button9 };
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
+            setTitle("Simple");
 
-            // 스레드 시작
-            btn_start = (Button)findViewById(R.id.btn_start);
-            btn_start.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
+            edit1 = (EditText)findViewById(R.id.input1);
+            edit2 = (EditText)findViewById(R.id.input2);
 
-                    isThread = true;
-                    thread = new Thread() {
-                        public void run() {
-                            while(isThread) {
-                                try {
-                                    sleep(5000);
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                }
-                                handler.sendEmptyMessage(0);
-                            }
+            btnAdd = (Button) findViewById(R.id.buttonAdd);
+            btnSub = (Button) findViewById(R.id.buttonSub);
+            btnMul = (Button) findViewById(R.id.buttonMul);
+            btnDiv = (Button) findViewById(R.id.buttonDiv);
+            textResult = (TextView) findViewById(R.id.textResult);
+
+            for (int i = 0; i<numBtnIDs.length; i++){
+                numButtons[i] = (Button)findViewById(numBtnIDs[i]);
+            }
+            for (int i=0; i<numBtnIDs.length; i++){
+                final int index;
+                index = i;
+                numButtons[index].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (edit1.isFocused() == true) {
+                            num1 = edit1.getText().toString()
+                                    + numButtons[index].getText().toString();
+                            edit1.setText(num1);
+                        } else if (edit2.isFocused() == true) {
+                            num2 = edit2.getText().toString()
+                                    + numButtons[index].getText().toString();
+                            edit2.setText(num2);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "먼저 에디트텍스트를 선택하세요", Toast.LENGTH_SHORT).show();
                         }
-                    };
-                    thread.start();
+                    }
+                });
+            }
+            btnAdd.setOnTouchListener(new View.OnTouchListener() {
+                public boolean onTouch(View arg0, MotionEvent arg1) {
+                    num1 = edit1.getText().toString();
+                    num2 = edit2.getText().toString();
+                    result = Double.parseDouble(num1) + Double.parseDouble(num2);
+                    textResult.setText("결과 : " + Double.toString(result));
 
+                    InputMethodManager manager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                    manager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+                    return false;
                 }
             });
 
-            // 스레드 종료
-            btn_stop = (Button)findViewById(R.id.btn_stop);
-            btn_stop.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    isThread = false;
+            btnAdd.setOnTouchListener(new View.OnTouchListener() {
+                public boolean onTouch(View arg0, MotionEvent arg1) {
+                    num1 = edit1.getText().toString();
+                    num2 = edit2.getText().toString();
+                    result = Double.parseDouble(num1) - Double.parseDouble(num2);
+                    textResult.setText("결과 : " + Double.toString(result));
+
+                    InputMethodManager manager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                    manager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+                    return false;
                 }
             });
 
+            btnAdd.setOnTouchListener(new View.OnTouchListener() {
+                public boolean onTouch(View arg0, MotionEvent arg1) {
+                    num1 = edit1.getText().toString();
+                    num2 = edit2.getText().toString();
+                    result = Double.parseDouble(num1) * Double.parseDouble(num2);
+                    textResult.setText("결과 : " + Double.toString(result));
+
+                    InputMethodManager manager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                    manager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+                    return false;
+                }
+            });
+
+            btnAdd.setOnTouchListener(new View.OnTouchListener() {
+                public boolean onTouch(View arg0, MotionEvent arg1) {
+                    num1 = edit1.getText().toString();
+                    num2 = edit2.getText().toString();
+                    result = Double.parseDouble(num1) / Double.parseDouble(num2);
+                    textResult.setText("결과 : " + Double.toString(result));
+
+                    InputMethodManager manager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                    manager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+                    return false;
+                }
+            });
         }
 
-        private Handler handler = new Handler(){
-            public void handlerMessage(Message msg){
-                Toast.makeText(getApplicationContext(), "Android", Toast.LENGTH_SHORT).show();
-            }
-        };
     }

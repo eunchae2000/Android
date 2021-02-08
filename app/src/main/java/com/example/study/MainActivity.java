016681a58ecb3174d0ad89b1d66057ad2b872390
coco <tldjs3651@naver.com>
2021-02-08 23:39:24 +0900

    Android Studio를 이용해서 영화 종목들을 투표하고 별점주고 투표 결과를 출력하기 - 3 <안드로이드 특강> + 부가설명

--- app/src/main/java/com/example/study/MainActivity.java
+++ app/src/main/java/com/example/study/MainActivity.java
@@ -12,6 +12,15 @@
 
     public class MainActivity extends AppCompatActivity {
 
+        /*
+        onCreate(@Nullable Bundle savedInstanceState) 메소드
+
+        => 액티비티가 최조 생성될 때 호출되는 메소(초기화 작업이 진행)
+        onCreate는 매개변수로 Bundle savedInstanceState를 받음
+        액티비티가 실행되면서 onCreate가 호출되면서 Bundle의 객체를 전달받음
+
+        Bundle => 동적상태의 데이터를 저장하고 복원하는 클래스
+         */
         @Override
         protected void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -47,7 +56,20 @@
             btnFinish.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    /*
+                    Intent
+                    intent는 다른 액티비티로 전환할 때 사용
+                    getApplicationContext()는 this와 비슷한 개념으로
+                    getApplicationContext는 application을 가리키는 것이고 this는 activity를 가리키는 것이라고 생각
+                     */
                     Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                    /*
+                    putExtra(key, value)
+                    putExtra는 Intent에 속하며 intent를 이용해 다른 화면으로 전환될 때 putExtra(key, value) key값을 전달받아 value값으로 반환하면 출력하는 것
+                    다른 액티비티로 이동하면서 putExtra의 key값을 통해서 vlaue값을 다른 액티비티에 출력
+
+                    그리고 버튼을 누르면 startActivity(intent) or startActivityForResult(intent)가 수행되며 메인 액티비티 위에 다른 액티비티가 실행
+                     */
                     intent.putExtra("VoteCount", voteCount);
                     intent.putExtra("ImageName", imgName);
                     startActivity(intent);
--- app/src/main/java/com/example/study/ResultActivity.java
+++ app/src/main/java/com/example/study/ResultActivity.java
@@ -20,9 +20,9 @@ public class ResultActivity extends AppCompatActivity {
 
         setTitle("투표 결과");
 
-        Intent intent = getIntent();
-        int[] voteResult = intent.getIntArrayExtra("VoteCount");
-        String[] imageName = intent.getStringArrayExtra("ImageName");
+        Intent intent = getIntent();    // MainActivity에서 선언한 intent를 받아주겠다는 의미
+        int[] voteResult = intent.getIntArrayExtra("VoteCount");    // VoteCount의 값을 int형으로 가져옴
+        String[] imageName = intent.getStringArrayExtra("ImageName");   // ImageName의 값을 string형으로 가져
 
         TextView textView[] = new TextView[imageName.length];
         RatingBar ratingBar[] = new RatingBar[imageName.length];

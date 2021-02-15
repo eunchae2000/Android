9b1af3c1f27d0a45dc7f5b6a438a532ebd2f4ec3
coco <tldjs3651@naver.com>
2021-02-15 21:06:34 +0900

    Android Studio를 이용해서 AlertDialog를 사용, 다이얼로그로 팝업창을 띄우는 것에 대해서 공부

---
+++ app/src/main/java/com/example/study/BootCompleteReciver.java
@@ -0,0 +1,14 @@
+package com.example.study;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class BootCompleteReciver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "부팅 완료", Toast.LENGTH_SHORT)
+    }
+}

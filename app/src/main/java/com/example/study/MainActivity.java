02dbae236a4620bef6a880777bbd6a06e5fa03a6
coco <tldjs3651@naver.com>
2021-02-20 20:36:02 +0900

    Android Studio로 Media Player를 이용하여 음악을 재생, 정지 실습

--- app/src/main/java/com/example/study/MainActivity.java
+++ app/src/main/java/com/example/study/MainActivity.java
@@ -1,59 +1,61 @@
     package com.example.study;
 
-    import androidx.annotation.NonNull;
-    import androidx.annotation.RequiresApi;
     import androidx.appcompat.app.AppCompatActivity;
 
-    import android.hardware.Camera;
-    import android.media.MediaRecorder;
-    import android.os.Build;
+
+    import android.media.MediaPlayer;
     import android.os.Bundle;
-    import android.view.SurfaceControl;
-    import android.view.SurfaceHolder;
-    import android.view.SurfaceView;
     import android.view.View;
-    import android.widget.AdapterView;
     import android.widget.Button;
-    import android.widget.Spinner;
-    import android.widget.TextView;
-    import android.widget.inline.InlineContentView;
+
 
     public class MainActivity extends AppCompatActivity {
+        
+        Button btn_play;
+        Button btn_stop;
 
-        /*
-        Spinner => 스피너는 값 집합에서 하나의 값을 선택할 수 있는 빠른 방법을 제공
+        MediaPlayer mediaPlayer;
 
-        기본 상태의 스피너는 현재 선택된 값을 표시.
-        스피너를 터치하면 기타 모든 사용 가능한 값을 포함하는 드롭다운 메뉴가 표시되며, 여기서 새 값을 선택할 수 있음.
-         */
-
-        private Spinner spinner;
-        private TextView tv_result;
+        // 액티비티가 종료될 때 이곳을 실행
+        @Override
+        protected void onDestroy() {
+            super.onDestroy();
+            if (mediaPlayer != null){
+                mediaPlayer.release();  // release = 해제
+                // release는 MediaPlayer 호출시 사용을 권장
+                mediaPlayer = null;
+            }
+        }
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
-            spinner = (Spinner)findViewById(R.id.spinner);
-            tv_result = (TextView)findViewById(R.id.tv_result);
+            btn_play = findViewById(R.id.btn_play);
+            btn_stop = findViewById(R.id.btn_stop);
 
-            // spinner 이벤트 처리 = setOnItemSelectedListener 이용
-            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
-                // 드롭다운이 선택되었을 때
+            // 재생 버튼을 눌렸을 때
+            btn_play.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                    // position으로 몇 번째 것이 선택되었는지 변수에 String 값으로 저장
-                    tv_result.setText(parent.getItemAtPosition(position).toString());
-                }
-
-                // 드롭다운이 선택되지 않았을 때
-                @Override
-                public void onNothingSelected(AdapterView<?> parent) {
-
+                public void onClick(View v) {
+                    mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.song1);
+                    mediaPlayer.start();
                 }
             });
+            
+            // 정지 버튼을 눌렸을 때
+            btn_stop.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // isPlaying은 지금 재생중인지 확인하는 것
+                    if(mediaPlayer.isPlaying()){
+                        mediaPlayer.stop();
+                        mediaPlayer.reset();
+                    }
+                }
+            });
+
 
         }
 

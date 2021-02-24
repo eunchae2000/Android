4aaf17d798f5a097915b9d2f4eea0bdf5d73a76c
coco <tldjs3651@naver.com>
2021-02-24 15:07:29 +0900

    Android Studio로 main xml의 LinearLayout을 실습

--- app/src/main/java/com/example/study/MainActivity.java
+++ app/src/main/java/com/example/study/MainActivity.java
@@ -13,41 +13,10 @@
 
     public class MainActivity extends AppCompatActivity {
 
-        private TextView tv_comeback;
-        private Button btn_go;
-
-        private  static final int REQUEST_CODE = 777;
-
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
-
-            tv_comeback = findViewById(R.id.tv_comeback);
-            btn_go = findViewById(R.id.btn_go);
-
-            btn_go.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent();
-                    startActivityForResult(intent, REQUEST_CODE);
-                }
-            });
         }
 
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-            super.onActivityResult(requestCode, resultCode, data);
-
-            if(resultCode == RESULT_OK){
-                Toast.makeText(getApplicationContext(), "수신 성공", Toast.LENGTH_SHORT).show();
-            }else{
-                Toast.makeText(getApplicationContext(), "수신 실패", Toast.LENGTH_SHORT).show();
-            }
-
-            if (requestCode == REQUEST_CODE){
-                String resultText = data.getStringExtra("comeback");
-                tv_comeback.setText(resultText);
-            }
-        }
     }

fdae6bd711bf915e5890c712c0c07f21b168d1ce
coco <tldjs3651@naver.com>
2021-03-03 17:45:59 +0900

    Android Studio로 RadioGroup으로 RadioButton을 그룹으로 묶어서 실행하는 실습과 관련 설명 추가

--- app/src/main/java/com/example/study/MainActivity.java
+++ app/src/main/java/com/example/study/MainActivity.java
@@ -1,39 +1,54 @@
     package com.example.study;
 
     import androidx.appcompat.app.AppCompatActivity;
-    import androidx.fragment.app.FragmentPagerAdapter;
-    import androidx.viewpager.widget.ViewPager;
 
     import android.os.Bundle;
+    import android.view.View;
+    import android.widget.Button;
+    import android.widget.RadioButton;
+    import android.widget.RadioGroup;
+    import android.widget.Toast;
 
-    import com.google.android.material.tabs.TabLayout;
 
     public class MainActivity extends AppCompatActivity {
 
-        // FragmentPagerAdapter => Fragment Manager 에서 관리하는 Fragment 를 나타내주는 것
-        /*
-        FragmentPagerAdapter => 제한된 페이지 개수들의 항목들에 적합 (fragment의 개수가 정해져있고 그 수가 많지 않을 때 사용)
-        FragmentStatePagerAdapter => 페이지 수를 알 수 없거나 페이지의 수가 많을 때 적합
-         */
-        private FragmentPagerAdapter fragmentPagerAdapter;
+        private RadioGroup rg_gender;
+        private RadioButton rb_women, rb_man;
+        private Button btn_result;
+        private String str_result;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
-            // 뷰페이저 세팅
-            // getSupportFragmentManager => 메인 액티비티에 새로 추가할 메소드로 프래그먼트 매니저를 이용해 프래그먼트를 전환하는 메소드
-            ViewPager viewPager = findViewById(R.id.viewPager);
-            fragmentPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+            rg_gender = findViewById(R.id.rg_gender);       // 라디오 버튼을 담고 있는 그룹
+            rb_man = findViewById(R.id.rb_man);             // 라디오 버튼
+            rb_women = findViewById(R.id.rb_women);         // 라디오 버튼
+            btn_result = findViewById(R.id.btn_result);     // 결과 값을 출력하라는 신호를 보낼 버튼
 
-            // setAdapter => viewPaper에 fragmentPaperAdapter를 적용하는 것
-            // setupWithViewPager => tabLayout과 viewPager은 연동시키는 것
-           TabLayout tabLayout = findViewById(R.id.tab_layout);
-            viewPager.setAdapter(fragmentPagerAdapter);
-            tabLayout.setupWithViewPager(viewPager);
+            rg_gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() { // 라디오 버튼들의 상태 값의 변경됨을 감지
+                @Override
+                public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                    if(i == R.id.rb_man){
+                        Toast.makeText(MainActivity.this, "남자 라디오 버튼", Toast.LENGTH_SHORT).show();
+                        str_result = rb_man.getText().toString();       // 라디오 버튼의 text값을 String에 담아 줌
+                    }else if(i == R.id.rb_women){
+                        Toast.makeText(MainActivity.this, "여자 라디오 버튼", Toast.LENGTH_SHORT).show();
+                        str_result = rb_women.getText().toString();     // 라디오 버튼의 text 값을 String에 담아 줌
+                    }
+                }
+            });
 
-
-
+            btn_result.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(str_result != null){     // str_result가 빈 값이 아니라면
+                        Toast.makeText(MainActivity.this, str_result, Toast.LENGTH_SHORT).show();
+                    }else{                      //  str_result가 빈 값이라면
+                        Toast.makeText(MainActivity.this, "라디오 버튼을 시작해주세요", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         }
     }
